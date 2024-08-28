66e8a2011129d483d38c68c03b9dacf72cbdd0e1
Jorge Raimundo <jorge.raimundo@gmail.com>
2024-08-28 11:10:23 +0100

    Cenas

--- QRParser-EFactura/Program.cs
+++ QRParser-EFactura/Program.cs
@@ -1,51 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Globalization;
+using QRParserEFactura;
 
 const string InputFile = "in.txt";
 const string OutputFile = "out.json";
 
-Dictionary<string,string> ElementNames = new() {
-    {"A", "NIF do emitente"},
-    {"B", "NIF do adquirente"},
-    {"C", "País do adquirente"},
-    {"D", "Tipo de documento"},
-    {"E", "Estado do documento"},
-    {"F", "Data do documento"},
-    {"G", "Identificação única do documento (nº. da factura)"},
-    {"H", "ATCUD"},
-    {"I1", "Espaço fiscal"},
-    {"I2", "Base tributável isenta de IVA"},
-    {"I3", "Base tributável de IVA à taxa reduzida (6%)"},
-    {"I4", "Total de IVA à taxa reduzida (6%)"},
-    {"I5", "Base tributável de IVA à taxa intermédia (13%)"},
-    {"I6", "Total de IVA à taxa intermédia (13%)"},
-    {"I7", "Base tributável de IVA à taxa normal (23%)"},
-    {"I8", "Total de IVA à taxa normal (23%)"},
-    {"J1", "Espaço fiscal"},
-    {"J2", "Base tributável isenta de IVA"},
-    {"J3", "Base tributável de IVA à taxa reduzida"},
-    {"J4", "Total de IVA à taxa reduzida"},
-    {"J5", "Base tributável de IVA à taxa intermédia"},
-    {"J6", "Total de IVA à taxa intermédia"},
-    {"J7", "Base tributável de IVA à taxa normal"},
-    {"J8", "Total de IVA à taxa normal"},
-    {"K1", "Espaço fiscal"},
-    {"K2", "Base tributável isenta de IVA"},
-    {"K3", "Base tributável de IVA à taxa reduzida"},
-    {"K4", "Total de IVA à taxa reduzida"},
-    {"K5", "Base tributável de IVA à taxa intermédia"},
-    {"K6", "Total de IVA à taxa intermédia"},
-    {"K7", "Base tributável de IVA à taxa normal"},
-    {"K8", "Total de IVA à taxa normal"},
-    {"L", "Não sujeito / não tributável em IVA"},
-    {"M", "Imposto do Selo"},
-    {"N", "Total de impostos"},
-    {"O", "Total do documento com impostos"},
-    {"P", "Retenções na fonte"},
-    {"Q", "Código de Controlo"},
-    {"R", "Número do Certificado"},
-    {"S", "Outras informações"}
+Dictionary<string,CodeElement> CodeElements = new() {
+    {"A", new CodeElement("NIF do emitente", 9)},
+    {"B", new CodeElement("NIF do adquirente", 30)},
+    {"C", new CodeElement("País do adquirente", 12)},
+    {"D", new CodeElement("Tipo de documento", 2)},
+    {"E", new CodeElement("Estado do documento", 1)},
+    {"F", new CodeElement("Data do documento", 8)},
+    {"G", new CodeElement("Identificação única do documento (nº. da factura)", 60)},
+    {"H", new CodeElement("ATCUD", 70)},
+    {"I1", new CodeElement("Espaço fiscal", 5)},
+    {"I2", new CodeElement("Base tributável isenta de IVA", 16)},
+    {"I3", new CodeElement("Base tributável de IVA à taxa reduzida (6%)", 16)},
+    {"I4", new CodeElement("Total de IVA à taxa reduzida (6%)", 16)},
+    {"I5", new CodeElement("Base tributável de IVA à taxa intermédia (13%)", 16)},
+    {"I6", new CodeElement("Total de IVA à taxa intermédia (13%)", 16)},
+    {"I7", new CodeElement("Base tributável de IVA à taxa normal (23%)", 16)},
+    {"I8", new CodeElement("Total de IVA à taxa normal (23%)", 16)},
+    {"J1", new CodeElement("Espaço fiscal", 1)},
+    {"J2", new CodeElement("Base tributável isenta de IVA", 16)},
+    {"J3", new CodeElement("Base tributável de IVA à taxa reduzida", 16)},
+    {"J4", new CodeElement("Total de IVA à taxa reduzida", 16)},
+    {"J5", new CodeElement("Base tributável de IVA à taxa intermédia", 16)},
+    {"J6", new CodeElement("Total de IVA à taxa intermédia", 16)},
+    {"J7", new CodeElement("Base tributável de IVA à taxa normal", 16)},
+    {"J8", new CodeElement("Total de IVA à taxa normal", 16)},
+    {"K1", new CodeElement("Espaço fiscal", 1)},
+    {"K2", new CodeElement("Base tributável isenta de IVA", 16)},
+    {"K3", new CodeElement("Base tributável de IVA à taxa reduzida", 16)},
+    {"K4", new CodeElement("Total de IVA à taxa reduzida", 16)},
+    {"K5", new CodeElement("Base tributável de IVA à taxa intermédia", 16)},
+    {"K6", new CodeElement("Total de IVA à taxa intermédia", 16)},
+    {"K7", new CodeElement("Base tributável de IVA à taxa normal", 16)},
+    {"K8", new CodeElement("Total de IVA à taxa normal", 16)},
+    {"L", new CodeElement("Não sujeito / não tributável em IVA", 16)},
+    {"M", new CodeElement("Imposto do Selo", 16)},
+    {"N", new CodeElement("Total de impostos", 16)},
+    {"O", new CodeElement("Total do documento com impostos", 16)},
+    {"P", new CodeElement("Retenções na fonte", 16)},
+    {"Q", new CodeElement("Código de Controlo", 4)},
+    {"R", new CodeElement("Número do Certificado", 4)},
+    {"S", new CodeElement("Outras informações", 65)}
 };
 
 if (!File.Exists(InputFile)) {
@@ -62,10 +63,18 @@ foreach (var line in fileLines)
     foreach (var element in elements)
     {
         List<string> elementParts = [.. element.Split(':')];
-        if (elementParts[0] == "F") 
-        {
-            elementParts[1] = DateTime.ParseExact(elementParts[1],"yyyyMMdd", CultureInfo.CurrentCulture).ToShortDateString();
+        var key = elementParts[0];
+        var data = elementParts[1];
+        var codeElement = CodeElements[key];
+
+        if (data.Length > codeElement.maxSize) {
+            throw new Exception($"O campo {key} ({codeElement.description}) ultrapassa o limite máximo de caracteres ({codeElement.maxSize}): {data}");
         }
-        Console.WriteLine($"{ElementNames[elementParts[0]]}: {elementParts[1]}");
+
+        if (key == "F") 
+        {
+            data = DateTime.ParseExact(data,"yyyyMMdd", CultureInfo.CurrentCulture).ToShortDateString();
+        }
+        Console.WriteLine($"{codeElement.description}: {data}");
     }
 }
